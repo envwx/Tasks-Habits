ceb6003ffdb3a32258d2facb57154df216a305a5
envwx <naz66cod@gmail.com>
2025-11-12 20:22:37 +0300

    added tasks model

--- TasksNHabits/main_app/models.py
+++ TasksNHabits/main_app/models.py
@@ -1,4 +1,18 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-# Create your models here.
+
+# Create your models he
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("task_details", kwargs={"task_id": self.id})
