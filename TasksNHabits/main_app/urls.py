457ade80b0b8c58c865487d16aeeabc2e7e50947
envwx <naz66cod@gmail.com>
2025-11-13 20:32:42 +0300

    implemented task functionality

--- TasksNHabits/main_app/urls.py
+++ TasksNHabits/main_app/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/signup/', views.signup, name='signup'),
     path('tasks/<int:task_id>/', views.task_details, name= 'task_details'),
+    path('tasks/', views.task_list, name='task_list'),
+    path('tasks/create/', views.task_create, name='task_create'),
+    path('tasks/<int:task_id>/edit/', views.task_edit, name='task_edit'),
+    path('tasks/<int:task_id>/delete/', views.task_delete, name='task_delete'),
+    path('tasks/<int:task_id>/complete/', views.task_complete, name='task_complete'),
 ]
--- TasksNHabits/main_app/views.py
+++ TasksNHabits/main_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
@@ -29,3 +31,54 @@ def signup(request):
 @login_required
 def task_details(request, task_id):
     task = Task.objects.get(id=task_id)
+    return render(request, 'tasks/task_details.html', {'task': task})
+
+def task_list(request):
+    tasks = Task.objects.filter(user=request.user)
+    return render(request, 'tasks/index.html', {'tasks': tasks})
+
+@login_required
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.user = request.user
+            task.save()
+            return redirect('task_list')
+
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/form.html', {'form': form})
+
+@login_required
+def task_edit(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task.user != request.user:
+        return redirect('task_list')
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_details', task_id=task.id)
+
+    else:
+        form = TaskForm(instance=task)
+
+        return render(request, 'tasks/form.html', {'form': form})
+
+@login_required
+def task_delete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task.user == request.user:
+        task.delete()
+        return redirect('task_list')
+
+@login_required
+def task_complete(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if task.user == request.user:
+        task.is_completed = True
+        task.save()
+        return redirect('task_details', task_id=task.id)
