e1bafb70ccbdf45aa103024bcda183d242cc6bb2
envwx <naz66cod@gmail.com>
2025-11-16 09:28:41 +0300

    added styling for base.html

--- TasksNHabits/TasksNHabits/settings.py
+++ TasksNHabits/TasksNHabits/settings.py
@@ -122,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "main_app" / "static"]
+
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = '/'
